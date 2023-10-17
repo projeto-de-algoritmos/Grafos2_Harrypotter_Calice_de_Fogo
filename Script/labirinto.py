1aef12cef59959f64d6a1d93746ded4dc4cd6cff
PedroMoraes39 <phcmoraes39@gmail.com>
2023-10-17 00:04:03 -0300

    Adicioando video

--- Script/labirinto.py
+++ Script/labirinto.py
@@ -2,7 +2,7 @@ import networkx as nx
 import random
 import matplotlib.pyplot as plt
 import tkinter as tk
-from algoritmo import dijkstra
+
 from tkinter import ttk
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
@@ -57,6 +57,44 @@ def calcular_caminho():
         canvas2.get_tk_widget().pack()
     else:
         resultado_label.config(text=f"Nó especificado não encontrado no labirinto.")
+
+def dijkstra(grafo, source, target):
+    dist = {node: float('inf') for node in grafo.nodes()}
+    dist[source] = 0
+    visited = set()
+
+    while visited != grafo.nodes():
+        min_node = None
+        for node in grafo.nodes():
+            if node not in visited:
+                if min_node is None:
+                    min_node = node
+                elif dist[node] < dist[min_node]:
+                    min_node = node
+
+        if min_node is None:
+            break
+
+        visited.add(min_node)
+        current_weight = dist[min_node]
+
+        for neighbor in grafo.neighbors(min_node):
+            weight = current_weight + grafo[min_node][neighbor]['weight']
+            if weight < dist[neighbor]:
+                dist[neighbor] = weight
+
+    if target not in dist:
+        return []
+
+    path = [target]
+    while target != source:
+        for neighbor in grafo.neighbors(target):
+            if dist[target] == dist[neighbor] + grafo[target][neighbor]['weight']:
+                target = neighbor
+                path.insert(0, target)
+                break
+
+    return path
 
 root = tk.Tk()
 root.title("Labirinto com Grafos e Pesos")
